2c82a41138fb4d7f4b7e447ce71aa3a4d90f1295
HetviThakkar-025 <hetvithakkar.019@gmail.com>
2025-06-07 18:49:26 +0530

    Server folders added and database connection done

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Connect to MongoDB Atlas
+connectDB();
+
+// Default test route
+app.get("/", (req, res) => {
+  res.send("API is working ✔️");
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
