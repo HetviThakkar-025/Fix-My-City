57aba264c8493c248476a11f1cabcfe7b03a87d1
HetviThakkar-025 <hetvithakkar.019@gmail.com>
2025-08-15 14:53:08 +0530

    minor changes

--- client/fixmycity/src/pages/wards/WardOfficerDashboard.jsx
+++ client/fixmycity/src/pages/wards/WardOfficerDashboard.jsx
@@ -232,7 +232,7 @@ const WardOfficerDashboard = () => {
             </button>
           </div>
 
-          <div className="hidden sm:ml-6 sm:fl.ex sm:flex-col sm:items-start gap-2">
+          <div className="sm:ml-6 sm:fl.ex sm:flex-col sm:items-start gap-2">
             <button
               onClick={handleLogout}
               className="text-gray-500 hover:text-gray-700 inline-flex items-center text-md font-medium mb-1"
